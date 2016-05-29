03e8a4b25e5fc5ee8158ed0797bfc09d5c17ad96
Tim Tasse <TasseT@cwu.edu>
2016-05-28 17:24:15 -0700

    Initial list of professors

---
+++ Professor.py
@@ -0,0 +1,24 @@
+class Professor:
+    def __init__(self, name, rating):
+        self.name = name
+        self.rating = rating
+
+    def __str__(self):
+        return self.name
+
+    def get_rating(self):
+        return self.rating
+
+montgomery = Professor("Montgomery", 7)
+andonie = Professor("Andonie", 7)
+kovalerchuk = Professor("Kovalerchuk", 7)
+davendra = Professor("Davendra", 7)
+vajda = Professor("Vajda", 7)
+harper = Professor("Harper", 7)
+guerinoni = Professor("Guerinoni", 7)
+abdulwahid = Professor("Abdul-Wahid", 7)
+harrison = Professor("Harrison", 7)
+lulofs = Professor("Lulofs", 7)
+salter = Professor("Salter", 7)
+schwing = Professor("Schwing", 7)
+popescu = Professor("Popescu", 7)
